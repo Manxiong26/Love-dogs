18365878217acd4f72cab4b85585b9c98759eb95
Maneena Xiong <xiong.maneena@gmail.com>
2021-08-04 13:17:38 -0500

    cleaning up codes and added comments

--- client.js
+++ client.js
@@ -1,5 +1,5 @@
-
-function myFunction() {
+// This function toggles the sidebar to hamburger menu
+function toggleFunction() {
   var x = document.getElementById("toprow");
   if (x.className === "row") {
     x.className += " responsive";

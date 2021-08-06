aeaaf6cf7e3c42d1d6712964495b60f08cf8f1f8
Maneena Xiong <xiong.maneena@gmail.com>
2021-08-06 01:15:16 -0500

    changed var to let

--- scripts/client.js
+++ scripts/client.js
@@ -1,6 +1,6 @@
 // This function toggles the sidebar to hamburger menu
 function toggleFunction() {
-  var x = document.getElementById("toprow");
+  let x = document.getElementById("toprow");
   if (x.className === "row") {
     x.className += " responsive";
   } else {
